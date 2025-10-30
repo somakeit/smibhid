9ebb669fc3f2ff1febc663f0232cd845ab5c5250
Stephen Jefferson <stephen@sjefferson.co.uk>
2025-10-30 18:57:45 +0000

    Remove firmware version from main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@
 Main file for the SMIB HID pico code. This file is responsible for starting the
 HID class and running.
 """
-# Built against Pico W Micropython firmware v1.22.2 https://micropython.org/download/RPI_PICO_W/
-
 from os import listdir
 
 updates_dir_list = []
