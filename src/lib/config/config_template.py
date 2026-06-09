89a9a2ee311216dbda1a864a1990fa77e4b23523
Stephen Jefferson <stephen@sjefferson.co.uk>
2026-06-09 18:49:14 +0100

    Add BH1750 I2C light sensor module to sensors.

--- src/lib/config/config_template.py
+++ src/lib/config/config_template.py
@@ -51,7 +51,7 @@ SCL_PIN = 9
 I2C_ID = 0
 I2C_FREQ = 400000
 
-## Sensors - Populate driver list with connected sensor modules from this supported list: ["SGP30", "BME280", "SCD30"]
+## Sensors - Populate driver list with connected sensor modules from this supported list: ["SGP30", "BME280", "SCD30", "BH1750"]
 SENSOR_MODULES = []
 
 # Default CO2 calibration value for SCD30 (global average is currently 427)
